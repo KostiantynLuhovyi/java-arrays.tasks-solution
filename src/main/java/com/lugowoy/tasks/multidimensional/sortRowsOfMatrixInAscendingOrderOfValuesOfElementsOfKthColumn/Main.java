30f3a7513f00e6c3b3ae779b0290fb7047f792ed
LugowoyKonstantin <kostya.lugowoy@gmail.com>
2018-10-26 22:21:29 +0300

    26.10.2018  22:17  • Solved task in the package com.lugowoy.tasks.multidimensional.sortRowsOfMatrixInAscendingOrderOfValuesOfElementsOfKthColumn;    Description task in the file sortRowsOfMatrixInAscendingOrderOfValuesOfElementsOfKthColumn.txt .  • Edit java version (10 to 1.9) in the pom.xml .

---
+++ src/main/java/com/lugowoy/tasks/multidimensional/sortRowsOfMatrixInAscendingOrderOfValuesOfElementsOfKthColumn/Main.java
@@ -0,0 +1,74 @@
+package com.lugowoy.tasks.multidimensional.sortRowsOfMatrixInAscendingOrderOfValuesOfElementsOfKthColumn;
+
+import com.lugowoy.helper.filling.matrixes.numbers.FillingMatrixRandomIntegerNumbers;
+import com.lugowoy.helper.io.reading.Reader;
+import com.lugowoy.helper.io.reading.ReadingConsole;
+import com.lugowoy.helper.models.Matrix;
+
+/**
+ * Created by Konstantin Lugowoy on 26.10.2018.
+ */
+
+public class Main {
+
+    private static final Reader READER = Reader.getReader(new ReadingConsole());
+
+    private static final int BOUND = 20;
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter number of rows in the matrix : ");
+        int rows = READER.readInt();
+
+        System.out.println("Enter number of columns in the matrix : ");
+        int columns = READER.readInt();
+
+        Matrix<Integer> matrix = Matrix.create(new FillingMatrixRandomIntegerNumbers().fill(rows, columns, BOUND));
+
+        int indexRowToSort = enterIndexRowToSort(rows);
+
+        System.out.println("Original matrix : ");
+        System.out.println(matrix);
+
+        sortRowsOfMatrixInAscendingOrderOfValuesOfElementsOfKthColumn(matrix, indexRowToSort);
+
+        System.out.println("Result : ");
+        System.out.println(matrix);
+
+    }
+
+    private static void sortRowsOfMatrixInAscendingOrderOfValuesOfElementsOfKthColumn(Matrix<Integer> matrix, int indexRowToSort) {
+        Integer[][] ints = matrix.getMatrix(new Integer[matrix.getRows()][matrix.getColumns()]);
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = i + 1; j < ints.length; j++) {
+                if (indexRowToSort >= 0 && indexRowToSort <= ints.length) {
+                    if (ints[i][indexRowToSort] > ints[j][indexRowToSort]) {
+                        for (int k = 0; k < ints.length; k++) {
+                            int temp = ints[i][k];
+                            ints[i][k] = ints[j][k];
+                            ints[j][k] = temp;
+                        }
+                    }
+                } else {
+                    System.err.println("Incorrect value.");
+                }
+            }
+        }
+        matrix.setMatrix(ints);
+    }
+
+    private static int enterIndexRowToSort(int rows) {
+        int resultIndexRow;
+        System.out.println("Enter the row number to sort : ");
+        while (true) {
+            resultIndexRow = READER.readInt();
+            if (resultIndexRow >= 0 && resultIndexRow <= rows) {
+                break;
+            } else {
+                System.out.println("Incorrect number. Re-enter : ");
+            }
+        }
+        return resultIndexRow;
+    }
+
+}
