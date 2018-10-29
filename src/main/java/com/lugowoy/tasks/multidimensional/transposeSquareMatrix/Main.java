fa72cf48d280becb58c5c5ba42164d43da4fb6ca
LugowoyKonstantin <kostya.lugowoy@gmail.com>
2018-10-29 21:23:46 +0200

    29.10.2018  21:23  • Solved task in the package com.lugowoy.tasks.multidimensional.transposeSquareMatrix;    Description task in the file transposeSquareMatrix.txt .

---
+++ src/main/java/com/lugowoy/tasks/multidimensional/transposeSquareMatrix/Main.java
@@ -0,0 +1,35 @@
+package com.lugowoy.tasks.multidimensional.transposeSquareMatrix;
+
+import com.lugowoy.helper.filling.matrixes.numbers.FillingMatrixRandomIntegerNumbers;
+import com.lugowoy.helper.models.Matrix;
+
+import static com.lugowoy.helper.filling.DefaultValuesForFilling.POSITIVE_INTEGER_BOUND;
+import static com.lugowoy.helper.models.Matrix.*;
+
+/**
+ * Created by Konstantin Lugowoy on 29.10.2018.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Matrix<Integer> matrix = create(new FillingMatrixRandomIntegerNumbers().fill(DEFAULT_ROWS, DEFAULT_COLUMNS, POSITIVE_INTEGER_BOUND));
+
+        System.out.println("Matrix : ");
+        System.out.println(matrix);
+
+        for (int i = 0; i < matrix.getRows(); i++) {
+            for (int j = i + 1; j < matrix.getColumns(); j++) {
+                int tmp = matrix.getElementMatrix(i, j);
+                matrix.setElementMatrix(i, j, matrix.getElementMatrix(j, i));
+                matrix.setElementMatrix(j, i, tmp);
+            }
+        }
+
+        System.out.println("Transpose matrix : ");
+        System.out.println(matrix);
+
+    }
+
+}
